019f1ef08266afd1b91167e3a51826f7d4cedea0
Ethan 『 Eltymme 』 <eltymme@outlook.fr>
2023-10-09 15:12:51 +0200

    [CLEAN] Project

--- src/events/ready/01registerCommands.js
+++ src/events/ready/01registerCommands.js
@@ -1,5 +1,3 @@
-const { Routes } = require('discord.js')
-const { testServer } = require('../../../config.json');
 const areCommandsDifferent = require('../../utils/areCommandsDifferent');
 const getApplicationCommands = require('../../utils/getApplicationCommands');
 const getLocalCommands = require('../../utils/getLocalCommands');
